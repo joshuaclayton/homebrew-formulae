4f66ddfdaf208f966276a6ebd95492e5d66bce7a
Joshua Clayton <joshua.clayton@gmail.com>
2016-05-22 06:44:25 -0400

    Update unused to v0.2.0.0

--- Formula/unused.rb
+++ Formula/unused.rb
@@ -5,9 +5,9 @@ class Unused < Formula
 
   desc "A command line tool to identify unused code."
   homepage "https://github.com/joshuaclayton/unused"
-  url "https://github.com/joshuaclayton/unused/archive/v0.1.1.0.tar.gz"
-  version "0.1.1.0"
-  sha256 "accbfc3f5eecee96609845ff82137fbe2d0bd587172785efdad883fb098b59b6"
+  url "https://github.com/joshuaclayton/unused/archive/v0.2.0.0.tar.gz"
+  version "0.2.0.0"
+  sha256 "7cab17e6356d274b1d1e553ec67f921f99f2934592d78ae4d94d7c2e8150824d"
   head "https://github.com/joshuaclayton/unused.git"
 
   depends_on "ghc" => :build
@@ -19,26 +19,35 @@ class Unused < Formula
   end
 
   test do
-    code = testpath/"foo.rb"
+    code = testpath/"awesome.rb"
     code.write <<-EOS.undent
-      once
-      twice
-      twice
-      twice
+      class Awesome
+        def once
+          twice
+        end
+
+        def twice
+          @twice || 2
+        end
+      end
     EOS
 
-    spec = testpath/"spec/foo_spec.rb"
+    spec = testpath/"spec/awesome_spec.rb"
     spec.write <<-EOS.undent
-      twice
-      twice
-      twice
+      require "spec_helper"
+
+      describe Awesome do
+        it "uses twice" do
+          expect(subject.once).to eq subject.twice
+        end
+      end
     EOS
 
-    output = shell_output("echo 'once\\ntwice\\nthrice\\n' | #{bin}/unused -a")
+    output = shell_output("echo 'once\\ntwice\\nthrice\\nonce\\n' | #{bin}/unused -a --stdin")
 
-    assert_match /once.*foo\.rb/, output
-    assert_match /twice.*foo\.rb/, output
-    assert_match /twice.*foo_spec\.rb/, output
+    assert_match /once.*awesome\.rb/, output
+    assert_match /twice.*awesome\.rb/, output
+    assert_match /twice.*awesome_spec\.rb/, output
     refute_match "thrice", output
 
     true
