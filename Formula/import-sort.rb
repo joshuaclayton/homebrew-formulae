b93bf33d4396a2424c66a2fb18bc0e667d1a6f4c
Joshua Clayton <joshua.clayton@gmail.com>
2016-07-31 07:28:12 -0400

    Release import-sort v0.3.0.0

--- Formula/import-sort.rb
+++ Formula/import-sort.rb
@@ -5,9 +5,9 @@ class ImportSort < Formula
 
   desc "A tool to sort Haskell imports"
   homepage "https://github.com/joshuaclayton/import-sort"
-  url "https://github.com/joshuaclayton/import-sort/archive/v0.2.0.0.tar.gz"
-  version "0.2.0.0"
-  sha256 "900df16e961cc8fe7fa72af98ac1fa358a7aa0b5418ff5c720eb05abde8aabe0"
+  url "https://github.com/joshuaclayton/import-sort/archive/v0.3.0.0.tar.gz"
+  version "0.3.0.0"
+  sha256 "b6dc907e7f6c8ff92d06d1f2fdbc221f9e6ad9db19d136ac58e49dcf5cfc273c"
   head "https://github.com/joshuaclayton/import-sort.git"
 
   depends_on "ghc" => :build
