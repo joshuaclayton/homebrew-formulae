d517938aba55cd281cf02f9647f3ad55306192a6
Joshua Clayton <joshua.clayton@gmail.com>
2017-01-12 08:43:06 -0500

    Update unused to v0.7.0.0

--- Formula/unused.rb
+++ Formula/unused.rb
@@ -5,9 +5,9 @@ class Unused < Formula
 
   desc "A command line tool to identify unused code."
   homepage "https://github.com/joshuaclayton/unused"
-  url "https://github.com/joshuaclayton/unused/archive/v0.6.1.0.tar.gz"
-  version "0.6.1.0"
-  sha256 "c4c894cbbb4712cc5ac8442f4bcb91dfc1e8d7ae6aac259eab476fb598285ef3"
+  url "https://github.com/joshuaclayton/unused/archive/v0.7.0.0.tar.gz"
+  version "0.7.0.0"
+  sha256 "ba6fc02bccf59f2d2d70e3b93e7168d8358f91d8704663eafc7dd712efb96251"
   head "https://github.com/joshuaclayton/unused.git"
 
   depends_on "ghc" => :build
